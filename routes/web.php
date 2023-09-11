d3f439a9972d5121d42ce2d341ee71d0df7c1315
Deep <thetestcoder@gmail.com>
2023-09-11 09:27:51 +0530

    fix: password reset issue

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\{
+    AuthenticatedSessionController,
+    NewPasswordController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('login', [AuthenticatedSessionController::class, 'create'])
     ->name('login');
 Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+    ->name('password.reset');
+
+Route::post('reset-password', [NewPasswordController::class, 'store'])
+    ->name('password.store');
