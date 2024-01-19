af8df18839cd03427e69a1287fe8e8bda27ac5c9
Avni Patel <avni@glorywebsdev.com>
2024-01-19 10:47:30 +0530

    Feat: Add is_user column in users table to identify user type

---
+++ database/migrations/2024_01_19_051353_add_is_user_field_in_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_user')->default(1)->comment('1-Seller,0-Customer/Buyer')->after('signup_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_user');
+        });
+    }
+};
